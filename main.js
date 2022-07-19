76fb38a5fc5c156fd26435c44499722eb934c8dc
benjamin sainz <hueroacapulco@gmail.com>
2022-07-18 20:27:44 -0500

    Build new func (game())

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-
+// this function use Math class to generate a random number which can change it to a string (rock,scissor or paper)
 function getComputerChoice(){
         let rdmNumber = Math.floor(Math.random() * 3);
 
@@ -27,44 +27,80 @@ function getComputerChoice(){
          }
 
 }
-//console.log(getComputerChoice());
-
-var playerSelection = "rock";
-console.log("your choice:        " + playerSelection);
-var computerSelection = getComputerChoice();
-console.log("computer choice:    " + computerSelection);
 
 
-
-console.log(playRound(playerSelection,computerSelection));
-
+//this function compares between 2 strings and return a string
 function playRound (playerSelection, computerSelection){
         if (playerSelection == computerSelection){
-                console.log ("tie");
+                return "tie";
         }
-        if (playerSelection == "rock" && computerSelection == "scissors"){
-                console.log ("You win");
+        else if (playerSelection == "rock" && computerSelection == "scissors"){
+                return "You win";
         
         }
-        if (playerSelection == "rock" && computerSelection == "paper"){
-                console.log ("You Lose! Paper beats Rock");
+        else if (playerSelection == "rock" && computerSelection == "paper"){
+               return "You Lose! Paper beats Rock";
         
         }
-        if (playerSelection == "scissors" && computerSelection == "paper"){
-                console.log ("You win");
+        else if (playerSelection == "scissors" && computerSelection == "paper"){
+                return "You win";
         
         }
-        if (playerSelection == "scissors" && computerSelection == "rock"){
-                console.log ("You Lose! Rock beats Scissors");
-        
-        }if (playerSelection == "paper" && computerSelection == "rock"){
-                console.log ("You win");
+        else if (playerSelection == "scissors" && computerSelection == "rock"){
+                return "You Lose! Rock beats Scissors";
         
         }
-        if (playerSelection == "paper" && computerSelection == "scissors"){
-                console.log ("You Lose! Scissors beats Paper");
+        else if (playerSelection == "paper" && computerSelection == "rock"){
+                return "You win";
+        
+        }
+        else if (playerSelection == "paper" && computerSelection == "scissors"){
+                return "You Lose! Scissors beats Paper";
         
         }
         
         }
+
+        //we call game which call both functions playRound and getComputerChoice
+        game();
+
+        //this function call function playRound (5 times) and return a game winner that 
+       function game (){
+                //get user input here 
+                var playerSelection = prompt("Please enter your choice here: ").toLowerCase();
+                var counterWins = 0;
+                var counterLoses =0;
+                for(var i = 0; i < 5; i++ ){
+                        console.log("game: "+ (i+1));
+                        console.log("your choice:        " + playerSelection);
+                        //every time we call getComputerChoice will generate new value
+                        var computerSelection = getComputerChoice();
+                        console.log("computer choice:    " + computerSelection);
+                        // we call playRound func 5 times 
+                        console.log(playRound(playerSelection,computerSelection));
+                        var stringResult = playRound(playerSelection,computerSelection);
+                        if (stringResult.includes('win')){
+                                counterWins++;
+                        }
+                        else if (stringResult.includes('Lose')){
+                                counterLoses++
+                        }
+                     
+                }
+                console.log("my total wins =" + counterWins);
+                console.log("my total loses =" + counterLoses);
+                //a final if conditional to show the winner
+                if (counterLoses > counterWins){
+                        console.log("Sorry, You Lost!!")
+                }
+                else if (counterWins > counterLoses){
+                        console.log("YOU ARE A WINNER!!")
+                }
+                else {
+                        console.log("ufff!!! Tie game");
+                }
+   
+                
+        }
+       
         
